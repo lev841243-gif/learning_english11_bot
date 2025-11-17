a19be65bb1ca69fefb8267d02d426f656eb6e311
lev841243-gif <lev841243@gmail.com>
2025-11-18 02:27:12 +0300

    fourth commit

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 DB_CONFIG = {
     'dbname': 'english_bot_db',
     'user': 'postgres',
-    'password': '05031981',
+    'password': '05031981', # вставить свой пароль
     'host': 'localhost',
     'port': '5432'
 }
 
 BOT_TOKEN = '8562502899:AAFgFSrPT2Ayg4SDyhIzXxUP4oBe5w-TO0Y'  # вставить свой токен
-#BOT_TOKEN = '8562502899:AAFgFSrPT2Ayg4SDyhIzXxUP4oBe5w-TO0Y'
